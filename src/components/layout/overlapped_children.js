a12137c6611491a8aa70cc963e342c016b4d1e6c
earyzhe <earyzhe@gmail.com>
2019-06-21 14:41:48 +0200

    Last commit before final project

--- src/components/layout/overlapped_children.js
+++ src/components/layout/overlapped_children.js
@@ -6,15 +6,14 @@ export class OverLappedChildren extends React.Component{
     constructor (props) {
         super(props);
         this.children = props.children;
-        this.style = {
-            position: "absolute",
-            transform: props.transform || "translateX(-50%)",
-            width: props.width,
-        };
     }
     render(){
         return (
-            <div style={this.style} className={'overLappedChildren'}>
+            <div style={{
+                position: "absolute",
+                transform: this.props.transform || "translateX(-50%)",
+                width: this.props.width,
+            }} className={'overLappedChildren'}>
                 {this.children}
             </div>
         );
